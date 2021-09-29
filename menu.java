d22ba025fc9dfd666bed900e7ca72c951a0b9a33
Sar2x <60858973+sjessicameyer@users.noreply.github.com>
2021-09-29 23:39:51 +0000

    super final!

--- menu.java
+++ menu.java
@@ -57,8 +57,9 @@ public class menu{
         }
         System.out.println("");
         for (int x=0;x<customMath.countDigits(num2);x++){
-          int num1Fragment=customMath.findDigit(num1, x);
-          System.out.println(mathStuff.convertFromBase10(mathStuff.doMath(mathStuff.convertToBase10(num1Fragment, base), mathStuff.convertToBase10(num2,base), operator),base));
+          int num2Fragment=customMath.findDigit(num2, x+1);
+          System.out.println(mathStuff.convertFromBase10(mathStuff.doMath(mathStuff.convertToBase10(num1, base), mathStuff.convertToBase10(num2Fragment,base), operator),base));
+          
         }
         for (int x=0;x<customMath.countDigits(answer);x++){
           System.out.print("=");
